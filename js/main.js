ff6d9460e86663883b83269cbd63e7098d4465ff
Gobinda Das <144037976+gdeditzyt@users.noreply.github.com>
2023-12-01 15:08:36 +0530

    remove mouse follower

--- js/main.js
+++ js/main.js
@@ -37,32 +37,3 @@ animate.reveal(".nav");
 animate.reveal(".home-text", {origin: "left"});
 animate.reveal(".home-img", {origin: "bottom"});
 animate.reveal(".ser-box, .product-box, .team-box, .book-data", {interval: 100});
-
-
-
-// Mouse move
-const ball1 = document.querySelector('#ball1');
-const ball2 = document.querySelector('#ball2');
-const ball3 = document.querySelector('#ball3');
-const ball4 = document.querySelector('#ball4');
-const ball5 = document.querySelector('#ball5');
-const ball6 = document.querySelector('#ball6');
-const ball7 = document.querySelector('#ball7');
-const ball8 = document.querySelector('#ball8');
-
-function ballPicker(ball, dets) {
-    ball.style.left = dets.pageX + "px";
-    ball.style.top = dets.pageY + "px";
-}
-
-document.querySelector('body').addEventListener('mousemove', (dets) => {
-    
-    ballPicker(ball1, dets);
-    ballPicker(ball2, dets);
-    ballPicker(ball3, dets);
-    ballPicker(ball4, dets);
-    ballPicker(ball5, dets);
-    ballPicker(ball6, dets);
-    ballPicker(ball7, dets);
-    ballPicker(ball8, dets);
-});
